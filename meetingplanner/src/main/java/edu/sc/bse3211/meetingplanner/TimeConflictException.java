ecb4c1e6ab80efe97a089b7cdb314d02e5f5e73b
Katongole Jonathan <67139624+jonakatin@users.noreply.github.com>
2026-05-02 07:07:44 +0300

    Update Maven configuration to use Java 21 and upgrade maven-compiler-plugin to version 3.13.0. Refactor TimeConflictException constructors to remove unnecessary comments.

--- meetingplanner/src/main/java/edu/sc/bse3211/meetingplanner/TimeConflictException.java
+++ meetingplanner/src/main/java/edu/sc/bse3211/meetingplanner/TimeConflictException.java
@@ -5,27 +5,23 @@ public class TimeConflictException extends Exception {
 	private static final long serialVersionUID = 8147822812157714367L;
 
 	public TimeConflictException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	public TimeConflictException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TimeConflictException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TimeConflictException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TimeConflictException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 }
